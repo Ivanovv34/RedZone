9deed318cbf66bac4138ae5a8507219ed7d06e32
Hristiyan Ivanov <hrisivanov4@gmail.com>
2026-03-14 19:30:39 +0200

    Add CompetitionController and views

---
+++ RedZone.Web/Controllers/CompetitionController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedZone.Services.Core.Interfaces;
+using RedZone.ViewModels.Competition;
+
+namespace RedZone.Web.Controllers
+{
+    public class CompetitionController : Controller
+    {
+        private readonly ICompetitionService competitionService;
+
+        public CompetitionController(ICompetitionService competitionService)
+        {
+            this.competitionService = competitionService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = await competitionService.GetAllAsync();
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CompetitionViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await competitionService.CreateAsync(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await competitionService.GetByIdAsync(id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, CompetitionViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var existingCompetition = await competitionService.GetByIdAsync(id);
+
+            if (existingCompetition == null)
+            {
+                return NotFound();
+            }
+
+            await competitionService.EditAsync(id, model);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var model = await competitionService.GetByIdAsync(id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var existingCompetition = await competitionService.GetByIdAsync(id);
+
+            if (existingCompetition == null)
+            {
+                return NotFound();
+            }
+
+            await competitionService.DeleteAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
