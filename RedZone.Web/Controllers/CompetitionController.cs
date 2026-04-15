7056b389dfe9844f758b8e4857efdf89e89d3292
Hristiyan Ivanov <hrisivanov4@gmail.com>
2026-04-15 16:07:12 +0300

    Move competition management to Admin area and fixed _ViewStart

--- RedZone.Web/Controllers/CompetitionController.cs
+++ RedZone.Web/Controllers/CompetitionController.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using RedZone.Services.Core.Interfaces;
-using RedZone.ViewModels.Competition;
 
 namespace RedZone.Web.Controllers
 {
@@ -19,92 +17,6 @@ namespace RedZone.Web.Controllers
         {
             var model = await competitionService.GetAllAsync();
             return View(model);
-        }
-
-        [HttpGet]
-        [Authorize(Roles = "Admin")]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Create(CompetitionViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-
-            await competitionService.CreateAsync(model);
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        [HttpGet]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Edit(int id)
-        {
-            var model = await competitionService.GetByIdAsync(id);
-
-            if (model == null)
-            {
-                return NotFound();
-            }
-
-            return View(model);
-        }
-
-        [HttpPost]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Edit(int id, CompetitionViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-
-            var existingCompetition = await competitionService.GetByIdAsync(id);
-
-            if (existingCompetition == null)
-            {
-                return NotFound();
-            }
-
-            await competitionService.EditAsync(id, model);
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        [HttpGet]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var model = await competitionService.GetByIdAsync(id);
-
-            if (model == null)
-            {
-                return NotFound();
-            }
-
-            return View(model);
-        }
-
-        [HttpPost]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var existingCompetition = await competitionService.GetByIdAsync(id);
-
-            if (existingCompetition == null)
-            {
-                return NotFound();
-            }
-
-            await competitionService.DeleteAsync(id);
-
-            return RedirectToAction(nameof(Index));
         }
     }
 }
